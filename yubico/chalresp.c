f8fb724c14e0fbc4c031131933c99b73eab6aa8d
Ondrej Holecek <ondrej.holecek@gmail.com>
2018-12-23 21:35:23 +0100

    minor chalresp cleanups

--- yubico/chalresp.c
+++ yubico/chalresp.c
@@ -7,19 +7,26 @@
 
 #include "yubico/error.h"
 #include "yubico/chalresp.h"
+#include "safe_mem.h"
+
 
 int tc_ykchl_hmac(int slot, unsigned char *pass, int passlen,
         unsigned char * result, char *errmsg)
 {
     int rv = 0;
+    unsigned char *response = NULL;
     YK_KEY *yk = 0;
 
     if (!yk_init()) CERROR(-1, "Yubikey chl. init failed");
 
     if (!(yk = yk_open_key(0))) CERROR(-1, "Yubikey chl. init failed");
 
-    unsigned char response[SHA1_MAX_BLOCK_SIZE];
-    memset(response, 0, sizeof(response));
+    response = alloc_safe_mem(SHA1_MAX_BLOCK_SIZE);
+
+    if (!response) CERROR(-1, "Error allocating memory for "
+            "Yubikey response");
+
+    memset(response, 0, SHA1_MAX_BLOCK_SIZE);
 
     int yk_cmd;
 
@@ -32,16 +39,16 @@ int tc_ykchl_hmac(int slot, unsigned char *pass, int passlen,
     }
 
     if (!yk_challenge_response(yk, yk_cmd, true, passlen,
-                pass, sizeof(response), response))
+                pass, SHA1_MAX_BLOCK_SIZE, response))
     {
         CERROR(-3, "Yubikey yk_challenge_response failed");
     }
 
     memcpy(result, response, YKCHL_RESPONSE_LENGTH);
-    memset(response, 0, sizeof(response));
 
 err:
     if (yk) yk_close_key(yk);
+    free_safe_mem(response);
 
     return rv;
 }
